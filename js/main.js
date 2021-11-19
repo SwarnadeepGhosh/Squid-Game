a524df60bc13bac3067b87720400a4ac0a0e845a
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-11-19 10:39:03 +0530

    Loading 3D Model, Doll class and GSAP Animation

--- js/main.js
+++ js/main.js
@@ -5,19 +5,60 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+renderer.setClearColor(0xb7c3f3, 1)
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light 
+scene.add( light );
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
 
+// Instantiate a loader
+const loader = new THREE.GLTFLoader();
+
+class Doll{
+    constructor(){
+        loader.load("../models/scene.gltf", (gltf) => {
+            scene.add(gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        })
+    }
+
+    lookBackward(){
+        // this.doll.rotation.y= -3.15;
+        gsap.to(this.doll.rotation, {y:-3.15, duration: .45})
+    }
+
+    lookForward(){
+        // this.doll.rotation.y= -3.15;
+        gsap.to(this.doll.rotation, {y:0, duration: .45})
+    }
+}
+
+let doll = new Doll();
+setTimeout(() => {
+    doll.lookBackward()
+}, 2000);
+
 function animate() {
     renderer.render( scene, camera );
-    
-    cube.rotation.x += 0.01;
+    // cube.rotation.x += 0.01;
 
     requestAnimationFrame( animate );
 }
 animate();
+
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize ( window.innerWidth, window.innerHeight);
+}
